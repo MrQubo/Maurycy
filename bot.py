6a67c389acd4eb86ca604ca3afc4acad723a2928
Jakub Nowak <jakub.jakub.nowak@gmail.com>
2021-05-26 02:48:25 +0200

    Add typing time and redirect to general
    And bunch of random fixes.

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import asyncio
 import os
 import random
 import string
@@ -8,12 +9,14 @@ import discord
 
 
 client = discord.Client()
-allowed_guild_ids = [
-    # WWI 2021
-    846678318928625684,
-]
 guild_id = 846678318928625684
 patryk_id = 654412645688016898
+redirect_to_general_channel_ids = [
+    # ustalenia-ważne
+    846678662102515733,
+]
+# ogólny
+general_channel_id = 846678318928625687
 
 message_count = 0
 
@@ -21,28 +24,44 @@ message_count = 0
 @client.event
 async def on_ready():
     for guild in client.guilds:
-        if guild.id not in allowed_guild_ids:
+        if guild.id != guild_id:
             await guild.leave()
+
+    global general_channel
+    general_channel = client.get_channel(general_channel_id)
 
 @client.event
 async def on_guild_join(guild):
-    if guild.id not in allowed_guild_ids:
+    if guild.id != guild_id:
         await guild.leave()
 
 @client.event
 async def on_message(message):
     global message_count
 
-    if message.guild is None or message.guild.id not in allowed_guild_ids:
+    if message.guild is None or message.guild.id != guild_id:
         return
 
     if message.author == client.user:
         return
 
+    if message.channel.id in redirect_to_general_channel_ids:
+        channel = general_channel
+    else:
+        channel = message.channel
+
     message_count += 1
     if message_count == 30:
+        typing_time = get_typing_time()
+        msg = get_funny_message()
+        # channel.typing() context manager is buggy
+        await channel.trigger_typing()
+        await asyncio.sleep(typing_time)
+        await channel.send(msg)
         message_count = 0
-        await message.channel.send(get_funny_message())
+
+def get_typing_time():
+    return min(random.expovariate(1/0.5) + 0.9, 4.0)
 
 funny_messages = [
     'dajcie admina',
@@ -57,21 +76,29 @@ def get_funny_message():
     from random import choice, randrange
 
     msg = choice(funny_messages)
-    if randrange(2) == 0:
+    if random_chance(1/2):
         msg = mutation_capitalize(msg)
-    if randrange(2) == 0:
+    if random_chance(1/2):
         msg = mutation_add_period(msg)
-    while randrange(20) == 0:
+    while random_chance(1/20):
         msg = mutation_swap(msg)
-    if randrange(20) == 0:
+    if random_chance(1/20):
         msg = mutation_switch_period(msg)
-    while randrange(30) == 0:
-        msg = mutation_switch_letter()
-    while randrange(25) == 0:
-            msg = mutation_skip_char()
-    if randrange(3) == 0:
+    while random_chance(1/50):
+        msg = mutation_switch_letter(msg)
+    while random_chance(1/25):
+        msg = mutation_skip_char(msg)
+    if random_chance(1/3):
         msg = mutation_mention_patryk(msg)
     return msg
+
+def random_chance(p):
+    assert 0 <= p <= 1
+    if p >= 1:
+        return True
+    if p <= 0:
+        return False
+    return random.uniform(0, 1) < p
 
 def mutation_capitalize(msg):
     return msg.capitalize()
@@ -98,7 +125,7 @@ def mutation_swap(msg):
     msg[idx], msg[idx+1] = msg[idx+1], msg[idx]
     return ''.join(msg)
 
-letters = frozenset(string.ascii_letters)
+letters = frozenset(string.ascii_letters + 'ęóąśłżźćĘÓĄŚŁŻŹĆ')
 def mutation_switch_letter(msg):
     assert len(msg) >= 1
     idx = random.randrange(len(msg))
